70141b01d66bb839bf6c03105aad3050ae32ce92
NoobieDIY <61545177+NoobieDIY@users.noreply.github.com>
2020-02-29 22:43:33 +0000

    Update RunTempLogger.sh

--- RunTempLogger.sh
+++ RunTempLogger.sh
@@ -3,6 +3,7 @@
 
 while true
 do
+ # Change the folder to the location you downloaded the script to
  python3 /root/TempLogger/TempLogger.py
  # Change this to change the interval between temperature readings, currently set to 5 seconds
  sleep 5
